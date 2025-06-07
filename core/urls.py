dfe27d5e4d2bf3939333bf5f1317150735ee1f89
Saket Singh <singhsaket513@gmail.com>
2025-06-07 21:31:36 +0530

    home Update

--- core/urls.py
+++ core/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.shortcuts import render
-
-from . import views
+from django.urls import path, include
+from .views import home
 
 urlpatterns = [
+    path('', home, name='home'),   # Homepage
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('sponsor/', include('sponsor_portal.urls')),
+    path('distributor/', include('distributor_portal.urls')),
+    path('qr/', include('qr_engine.urls')),
+    path('core-admin/', include('core_admin.urls')),
 ]
