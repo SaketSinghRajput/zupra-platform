bdb63a1addcbed517cb179078f7ca28b33ac31ff
Saket Singh <singhsaket513@gmail.com>
2025-06-08 12:32:31 +0530

    html fixing

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import home
 
+
 urlpatterns = [
     path('', home, name='home'),   # Homepage
     path('admin/', admin.site.urls),
